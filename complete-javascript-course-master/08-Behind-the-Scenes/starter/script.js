06d699899c0e105adb74ae50f118218974ddf02f
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2021-10-12 19:32:32 +0300

    Js

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+function calAge(birthYear){
+    const age=2037-birthYear;
+    
+    function printAge(){
+        const output=`You are ${age}, born in ${birthYear}`;
+        console.log(output);
+    }
+    printAge();
+    return age;
+}
+
+
+const firstName='Berke';
+calAge(1991);
--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order:function(starterIndex,mainIndex){
+    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,22 @@ const restaurant = {
     },
   },
 };
+ 
+const arr=[2,3,4];
+const a=arr[0];
+const b=arr[1];
+const c=arr[2];
+
+const[x,y,z]=arr;
+console.log(x,y,z);
+console.log(arr);
+
+const[first,,second]=restaurant.categories;
+console.log(first,second);
+
+const [starter,mainCourse]=restaurant.order(2,0);
+console.log(starter,mainCourse);
+
+const nested=[2,4,[5,6]];
+const[i,,[j,k]]=nested;
+console.log(i,k,j);
